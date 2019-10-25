a0e6afd45e82ad3086280f14b6d879e0c6ccb096
László Nits <nitslaszlo@gmail.com>
2019-10-25 16:12:19 +0200

    up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,24 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
+  env: {
     "node": true
   },
-  "extends": [
+  extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended"
   ],
-  "globals": {
+  globals: {
     "Atomics": "readonly",
     "SharedArrayBuffer": "readonly"
   },
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
     "ecmaVersion": 2018,
     "sourceType": "module"
   },
-  "plugins": [
+  plugins: [
     "@typescript-eslint"
   ],
-  "rules": {
+  rules: {
     "semi": [
       "error",
       "always"
