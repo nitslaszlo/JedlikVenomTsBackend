2d66061cf0708fbba1015387b512bb66eeee462b
László Nits <nitslaszlo@gmail.com>
2019-11-05 21:50:16 +0100

    up

--- dist/server.js
+++ dist/server.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 const app_1 = tslib_1.__importDefault(require("./app"));
 const PORT = 3000;
-app_1.default.listen(PORT, () => {
+app_1.default.listen(process.env.PORT || PORT, () => {
     console.log(`Express server listening on port: ${PORT}, if you get an error, check your mongoDB connection.`);
 });
 //# sourceMappingURL=server.js.map
