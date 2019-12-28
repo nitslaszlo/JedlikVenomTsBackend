84a22652fdb2be39089ad00a2db02223c492acf5
nitslaszlo <nitslaszlo@gmail.com>
2019-12-28 15:13:46 +0100

    up

--- dist/app.js
+++ dist/app.js
@@ -22,7 +22,7 @@ class App {
         this.app.use((req, res, next) => {
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-            res.header("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
+            res.header("Set-Cookie", "HttpOnly;Secure;SameSite=None");
             if (req.method === "OPTIONS") {
                 res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
                 return res.status(200).json({});
