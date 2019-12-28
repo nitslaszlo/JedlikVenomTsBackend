013ead44160a866ee997b0ff9c421296f533edb2
nitslaszlo <nitslaszlo@gmail.com>
2019-12-28 13:20:07 +0100

    uú

--- src/app.ts
+++ src/app.ts
@@ -51,7 +51,7 @@ class App {
       // Ha "Access-Control-Allow-Credentials", "true", akkor az origin nem lehet "*"!
       // res.header("Access-Control-Allow-Credentials", "true");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-      res.header("Set-Cookie", "HttpOnly;SameSite=None");
+      res.header("Set-Cookie", "HttpOnly;Secure;SameSite=None");
       res.header("Content-Security-Policy", "block-all-mixed-content=false");
       if (req.method === "OPTIONS") {
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
