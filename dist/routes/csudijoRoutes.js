d8cb30e4a64e9ba3cb1a652c1e7e593135a3ec7f
nitslaszlo <nitslaszlo@gmail.com>
2019-12-28 15:10:52 +0100

    up

---
+++ dist/routes/csudijoRoutes.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const csudijoController_1 = require("../controllers/csudijoController");
+class CsudijoRoutes {
+    constructor() {
+        this.csudijoController = new csudijoController_1.CsudijoController();
+    }
+    routes(app) {
+        app
+            .route("/csudijo")
+            .get((req, res, next) => {
+            console.log(`Request type ${req.method} from: ${req.originalUrl} time: ${new Date().toLocaleTimeString()}`);
+            next();
+        }, this.csudijoController.getAllFoods)
+            .post((req, res, next) => {
+            console.log(`Request type ${req.method} from: ${req.originalUrl} time: ${new Date().toLocaleTimeString()}`);
+            next();
+        }, this.csudijoController.addNewFood);
+        app.route("/csudijobest").get((req, res, next) => {
+            console.log(`Request type ${req.method} from: ${req.originalUrl} time: ${new Date().toLocaleTimeString()}`);
+            next();
+        }, this.csudijoController.getTopFoods);
+        app
+            .route("/csudijo/:foodId")
+            .get((req, res, next) => {
+            console.log(`Request type ${req.method} from: ${req.originalUrl} time: ${new Date().toLocaleTimeString()}`);
+            next();
+        }, this.csudijoController.getFoodWithID)
+            .put((req, res, next) => {
+            console.log(`Request type ${req.method} from: ${req.originalUrl} time: ${new Date().toLocaleTimeString()}`);
+            next();
+        }, this.csudijoController.updateFood)
+            .delete((req, res, next) => {
+            console.log(`Request type ${req.method} from: ${req.originalUrl} time: ${new Date().toLocaleTimeString()}`);
+            next();
+        }, this.csudijoController.deleteFood);
+    }
+}
+exports.CsudijoRoutes = CsudijoRoutes;
+//# sourceMappingURL=csudijoRoutes.js.map
