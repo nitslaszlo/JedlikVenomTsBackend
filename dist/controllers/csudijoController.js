c7fe8b976139f764d9b074ea46a9f9a35ddb51fd
nitslaszlo <nitslaszlo@gmail.com>
2020-11-17 12:07:28 +0100

    up

--- dist/controllers/csudijoController.js
+++ dist/controllers/csudijoController.js
@@ -1,92 +1,85 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.CsudijoController = void 0;
 const tslib_1 = require("tslib");
 const mongoose_1 = tslib_1.__importDefault(require("mongoose"));
 const csudijoModel_1 = require("../models/csudijoModel");
 const mongooseCsudijo = mongoose_1.default.model("Csudijo", csudijoModel_1.csudijoSchema);
 class CsudijoController {
-    addNewFood(req, res) {
-        const newFood = new mongooseCsudijo(req.body);
-        newFood.save((err, food) => {
-            if (err) {
-                res.send(err);
-            }
-            else {
-                res.json(food);
-            }
-        });
-    }
-    getAllFoods(req, res) {
-        mongooseCsudijo.find({}, (err, food) => {
-            if (err) {
-                res.send(err);
-            }
-            else {
-                res.json(food);
-            }
-        });
-    }
-    getTopFoods(req, res) {
-        mongooseCsudijo
-            .find({})
-            .sort({ numberOfVote: "desc" })
-            .limit(1)
-            .exec((err, food) => {
-            if (err) {
-                res.send(err);
-            }
-            else {
-                if (food.length > 0) {
-                    const max = food[0].numberOfVote;
-                    mongooseCsudijo.find({ numberOfVote: max }, (error, foods) => {
-                        if (error) {
-                            res.send(error);
-                        }
-                        else {
-                            res.json(foods);
-                        }
-                    });
-                }
-                else {
-                    res.json({ error: "No food!" });
-                }
-            }
-        });
-    }
-    getFoodWithID(req, res) {
-        mongooseCsudijo.findById(req.params.foodId, (err, food) => {
-            if (err) {
-                res.send(err);
-            }
-            else {
-                res.json(food);
-            }
-        });
-    }
-    updateFood(req, res) {
-        const updateOptions = {
-            new: true,
-            runValidators: true
-        };
-        mongooseCsudijo.findOneAndUpdate({ _id: req.params.foodId }, req.body, updateOptions, (err, food) => {
-            if (err) {
-                res.send(err);
-            }
-            else {
-                res.json(food);
-            }
-        });
-    }
-    deleteFood(req, res) {
-        mongooseCsudijo.deleteOne({ _id: req.params.foodId }, err => {
-            if (err) {
-                res.send(err);
-            }
-            else {
-                res.json({ message: "Successfully deleted food!" });
-            }
-        });
-    }
+  addNewFood(req, res) {
+    const newFood = new mongooseCsudijo(req.body);
+    newFood.save((err, food) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json(food);
+      }
+    });
+  }
+  getAllFoods(req, res) {
+    mongooseCsudijo.find({}, (err, food) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json(food);
+      }
+    });
+  }
+  getTopFoods(req, res) {
+    mongooseCsudijo
+      .find({})
+      .sort({ numberOfVote: "desc" })
+      .limit(1)
+      .exec((err, food) => {
+        if (err) {
+          res.send(err);
+        } else {
+          if (food.length > 0) {
+            const max = food[0].numberOfVote;
+            mongooseCsudijo.find({ numberOfVote: max }, (error, foods) => {
+              if (error) {
+                res.send(error);
+              } else {
+                res.json(foods);
+              }
+            });
+          } else {
+            res.json({ error: "No food!" });
+          }
+        }
+      });
+  }
+  getFoodWithID(req, res) {
+    mongooseCsudijo.findById(req.params.foodId, (err, food) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json(food);
+      }
+    });
+  }
+  updateFood(req, res) {
+    const updateOptions = {
+      new: true,
+      runValidators: true
+    };
+    mongooseCsudijo.findOneAndUpdate({ _id: req.params.foodId }, req.body, updateOptions, (err, food) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json(food);
+      }
+    });
+  }
+  deleteFood(req, res) {
+    mongooseCsudijo.deleteOne({ _id: req.params.foodId }, err => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json({ message: "Successfully deleted food!" });
+      }
+    });
+  }
 }
 exports.CsudijoController = CsudijoController;
 //# sourceMappingURL=csudijoController.js.map
--- dist/models/csudijoModel.js
+++ dist/models/csudijoModel.js
@@ -1,28 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.csudijoSchema = void 0;
 const tslib_1 = require("tslib");
 const mongoose_1 = tslib_1.__importDefault(require("mongoose"));
 const csudijoSchemaOptions = {
-    collection: "CsudijoFoodCollection",
-    versionKey: false
+  collection: "CsudijoFoodCollection",
+  versionKey: false
 };
-exports.csudijoSchema = new mongoose_1.default.Schema({
+exports.csudijoSchema = new mongoose_1.default.Schema(
+  {
     foodName: {
-        default: "Lecsó kolbászcsipszel",
-        required: "Kérem adjon meg egy étel nevet!",
-        type: String,
-        unique: true
+      default: "Lecsó kolbászcsipszel",
+      required: "Kérem adjon meg egy étel nevet!",
+      type: String,
+      unique: true
     },
     numberOfVote: {
-        default: 0,
-        min: 0,
-        type: Number,
-        get: (v) => Math.round(v),
-        set: (v) => Math.round(v)
+      default: 0,
+      min: 0,
+      type: Number,
+      get: v => Math.round(v),
+      set: v => Math.round(v)
     },
     createdDate: {
-        default: Date.now,
-        type: Date
+      default: Date.now,
+      type: Date
     }
-}, csudijoSchemaOptions);
+  },
+  csudijoSchemaOptions
+);
 //# sourceMappingURL=csudijoModel.js.map
