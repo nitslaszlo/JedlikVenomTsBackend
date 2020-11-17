ccd9046e32e45aad1b19aa9f182e9b86e08f3795
nitslaszlo <nitslaszlo@gmail.com>
2020-11-17 12:09:46 +0100

    up

--- dist/routes/crmRoutes.js
+++ dist/routes/crmRoutes.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.CrmRoutes = void 0;
 const crmController_1 = require("../controllers/crmController");
 class CrmRoutes {
     constructor() {
